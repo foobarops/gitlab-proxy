d8dda1e0c422677a0b27d83baa1c1bc4b81e7a4a
Andrei Bochkov <foobarops@gmail.com>
2024-08-08 14:18:55 +0000

    enable properties only for default config

--- src/main/java/com/example/gitlabproxy/config/GitlabConfig.java
+++ src/main/java/com/example/gitlabproxy/config/GitlabConfig.java
@@ -4,14 +4,18 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.gitlabproxy.api.GitLabGroupsApi;
 
 @Configuration
 @EnableCaching
-@EnableConfigurationProperties(GitLabGroupsApi.Config.class)
 public class GitlabConfig {
+    
+    @Profile("default")
+    @EnableConfigurationProperties(GitLabGroupsApi.Config.class)
+    public static class DefaultConfig {}
 
     @Bean
     public RestTemplate restTemplate() {
