54d4061884b7541d47dbac9fe85dc9ff1091e86f
Andrei Bochkov <foobarops@gmail.com>
2024-08-24 12:32:17 +0000

    comments

--- gradle-jar/bootJar.tasks.gradle.kts
+++ gradle-jar/bootJar.tasks.gradle.kts
@@ -1,5 +1,8 @@
 tasks.withType<Jar> {
     this.archiveFileName.set("app.jar")
+    
+    // In order to add the external folder to classpath we use PropertiesLauncher
     this.manifest.attributes["Main-Class"] = "org.springframework.boot.loader.PropertiesLauncher"
+    // This specifies the path where additional resources (like configuration files) can be found when the application is running.
     this.manifest.attributes["Loader-Path"] = "/app/resources"
 }
